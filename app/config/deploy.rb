65b3cb878552a46f2f914395481a0f591bd23d53
siddhartha.mahata <siddhartha.mahata@gmail.com>
2015-10-14 23:28:58 +0530

    vultr deploy new server capvagrant

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,10 +1,11 @@
 set :application, "test5"
-set :domain,      "192.168.33.10"
-set :user,       "vagrant"
+set :domain,      "45.63.52.40"
+set :user,       "root"
+set :use_sudo,    false
 set :deploy_to,   "/var/www/"
 set :app_path,    "app"
 
-set :repository,  "file:///C:/xampp/htdocs/capvagrant"
+set :repository,  "https://github.com/sidmahata/test5.git"
 set :deploy_via,   :copy
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
@@ -15,9 +16,15 @@ set :model_manager, "doctrine"
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
-set  :use_composer,     true
+set  :shared_files,     ["app/config/parameters.yml"]
+set  :shared_children,  [app_path + "/logs", web_path + "/uploads"]
 
+set  :use_composer,     true
+set  :update_vendors,   true
 set  :keep_releases,  3
+
+
+after "deploy:setup", "upload_parameters"
 
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
