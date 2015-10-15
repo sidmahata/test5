ee749d7f553f190425c62147983611f0854b93f3
siddhartha.mahata <siddhartha.mahata@gmail.com>
2015-10-15 21:59:27 +0530

    deploy to vagrant

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -19,11 +19,11 @@ role :app,        domain, :primary => true       # This may be the same as your 
 set  :shared_files,     ["app/config/parameters.yml"]
 set  :shared_children,  [app_path + "/logs", web_path + "/uploads"]
 
-set  :use_composer,     true
+#set  :use_composer,     true
 #set  :update_vendors,   true
 set  :keep_releases,  3
 
-set :composer_options, "--no-dev --verbose --prefer-dist --optimize-autoloader --no-progress"
+#set :composer_options, "--no-dev --verbose --prefer-dist --optimize-autoloader --no-progress"
 
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
